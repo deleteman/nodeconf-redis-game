ee12996e2e4bbe4f27230ad901709a893faffa51
Fernando Doglio <deleteman@gmail.com>
2017-10-25 22:59:28 -0300

    minor fixes

--- bots/bot1.js
+++ bots/bot1.js
@@ -4,19 +4,24 @@ const request = require("request"),
 const usernames = ["fernando", "deleteman", "andres", "juan", "carlos"];
 
 const ports = [9090, 9091];
+const totalRequests = 100;
 
 
-async.map(usernames, (usr, done1) => {
-	async.map(ports, (p, done2) => {
-		console.log("Querying port: "+ p);
-		request({
-			url: 'http://localhost:' + p + '/',
-			method: 'POST',
-			json: {username: usr}
-		}, done2)
-	}, done1)
+async.times(totalRequests, (n, next) => {
+	async.map(usernames, (usr, done1) => {
+		async.map(ports, (p, done2) => {
+			console.log("Querying port: "+ p);
+			request({
+				url: 'http://localhost:' + p + '/',
+				method: 'POST',
+				json: {username: usr}
+			}, done2)
+		}, done1)
+	}, () => {
+		console.log("FINISHED ITERATION ", n);
+		next();
+	})
 }, () => {
-	console.log("FINISHED!");
 	request({
 		url: 'http://localhost:' + ports[0] + '/ranking'
 	}, (err, resp, body) => {
