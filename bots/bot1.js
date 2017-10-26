2ebf9490739ef5f7e50613b70f1d3bd869ff1672
Fernando Doglio <deleteman@gmail.com>
2017-10-26 02:04:15 -0300

    fixing bug, adding comments and cleaning up the code

--- bots/bot1.js
+++ bots/bot1.js
@@ -4,27 +4,33 @@ const request = require("request"),
 const usernames = ["fernando", "deleteman", "andres", "juan", "carlos"];
 
 const ports = [9090, 9091];
-const totalRequests = 100;
+const totalRequests = 10;
 
-
-async.times(totalRequests, (n, next) => {
-	async.map(usernames, (usr, done1) => {
-		async.map(ports, (p, done2) => {
-			console.log("Querying port: "+ p);
-			request({
-				url: 'http://localhost:' + p + '/',
-				method: 'POST',
-				json: {username: usr}
-			}, done2)
-		}, done1)
+/*
+This bot hits all servers 10 times every 2 seconds
+The expected behavior is that every 2 seconds, each user will have its score increased by 1
+(all extra hits will be ignored by the server)
+*/
+setInterval(() => {
+	async.times(totalRequests, (n, next) => {
+		async.map(usernames, (usr, done1) => {
+			async.map(ports, (p, done2) => {
+				console.log("Querying port: "+ p);
+				request({
+					url: 'http://localhost:' + p + '/',
+					method: 'POST',
+					json: {username: usr}
+				}, done2)
+			}, done1)
+		}, () => {
+			console.log("FINISHED ITERATION ", n);
+			next();
+		})
 	}, () => {
-		console.log("FINISHED ITERATION ", n);
-		next();
+		request({
+			url: 'http://localhost:' + ports[0] + '/ranking'
+		}, (err, resp, body) => {
+			console.log(body)
+		})
 	})
-}, () => {
-	request({
-		url: 'http://localhost:' + ports[0] + '/ranking'
-	}, (err, resp, body) => {
-		console.log(body)
-	})
-})
+}, 2000);
--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,14 @@ let LOGGER = null;
 router.post('/', function(req, res, next) {
 	LOGGER.info("POST RECEIVED FROM USER: " + req.body.username);
 	Server.receiveHit(req.body, (err) => {
-		if(err) return res.json(err);
+		if(err) return next(err);
 		res.json({ result: 'OK'});
 	});
 });
 
 router.get('/ranking', function(req, res, next) {
 	LOGGER.info("GETTING RANKING");
-	res.json(Server.localRanking);
+		res.json(Server.localRanking);
 })
 module.exports = (logger, server) => {
 	Server = server;
